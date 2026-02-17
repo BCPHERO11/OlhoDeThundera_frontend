1faf012184a2487c60d4d9025dd339ab982c2996
Breno Copeland Pitanga <84689861+BCPHERO11@users.noreply.github.com>
2026-02-17 14:32:44 -0300

    Fix blank screen startup and harden occurrence parsing

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
 
-if (window.location.pathname === "/") {
-    window.history.replaceState({}, "", "/home");
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
